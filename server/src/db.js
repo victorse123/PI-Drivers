3fcec14ff80e404e117fba550a39df0623c50d24
Victor Sepulveda <victorseva123@gmail.com>
2023-12-18 21:04:45 -0500

    Actual 18/12/23

--- server/src/db.js
+++ server/src/db.js
@@ -3,9 +3,7 @@ const { Sequelize } = require("sequelize");
 
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/drivers`, {
   logging: false, 
