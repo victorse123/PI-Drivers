ecdc2af062ff0c1885dc3297e7f404058918f643
Victor Sepulveda <victorseva123@gmail.com>
2023-12-20 07:17:40 -0500

    Actual 20/12/23

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
-const axios = require("axios");
-const server = require("./src/server");
-const { conn } = require('./src/db.js');
-const PORT = 3001;
+// configuracion del servidor y sincronizacion con la base de datos
 
-conn.sync({ force: true }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+const server = require("./src/server");
+const { conn } = require("./src/db.js");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+conn
+  .sync({ force: false })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
