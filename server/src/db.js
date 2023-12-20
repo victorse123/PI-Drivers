2ff1883417fea0b4700b98a1d2b3d7ad7de2d3e6
Victor Sepulveda <victorseva123@gmail.com>
2023-12-20 07:05:08 -0500

    Actual 20/12/23

--- server/src/db.js
+++ server/src/db.js
@@ -1,37 +1,62 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+// const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const dataBaseDeploy = process.env.DATA_BASE_DEPLOY;
 
-const fs = require('fs');
-const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const DriverModel = require("./models/Driver");
+const TeamModel = require("./models/Team");
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/drivers`, {
-  logging: false, 
-  native: false, 
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/drivers`,
+//   {
+//     logging: false,
+//     native: false,
+//   }
+// );
+
+const sequelize = new Sequelize(dataBaseDeploy, {
+  logging: false,
+  native: false,
 });
-const basename = path.basename(__filename);
 
+// Obtener el nombre del archivo actual
+const basename = path.basename(__filename);
 const modelDefiners = [];
 
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+// Leer los archivos del directorio "models" y filtrar aquellos que son modelos
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  // Importar cada modelo y añadirlo al array modelDefiners
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
-
-modelDefiners.forEach(model => model(sequelize));
-
+// Iterar sobre los modelos y llamar a la función de definición para cada uno
+modelDefiners.forEach((model) => model(sequelize));
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { Driver } = sequelize.models;
+DriverModel(sequelize);
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+TeamModel(sequelize);
+
+const { Driver, Team } = sequelize.models;
+
+Driver.belongsToMany(Team, { through: "DriverTeam", timestamps: false });
+Team.belongsToMany(Driver, { through: "DriverTeam", timestamps: false });
 
 module.exports = {
+  Driver,
+  Team,
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
